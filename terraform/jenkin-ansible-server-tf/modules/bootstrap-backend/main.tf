7b56f6c55bbc520fdcf3ad252cb6192588f05ca1
Sourabh <devopsourabhbiswas.com>
2025-08-27 07:24:36 +0530

    update .gitignore for tfpaln

--- terraform/jenkin-ansible-server-tf/modules/bootstrap-backend/main.tf
+++ terraform/jenkin-ansible-server-tf/modules/bootstrap-backend/main.tf
@@ -49,19 +49,21 @@ resource "aws_s3_bucket_acl" "tf_state_acl" {
   ]
 }
 
-resource "aws_dynamodb_table" "tf_lock" {
-  name         = var.lock_table_name
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
+#Terraform cli version above 1.10+ does not require a DynamoDB table for state locking. Now S3 directly supports it. Enable it at S3.
 
-  tags = {
-    Name        = "Terraform Lock Table"
-    Environment = var.environment
-  }
-}
+#resource "aws_dynamodb_table" "tf_lock" {
+  #name         = var.lock_table_name
+  #billing_mode = "PAY_PER_REQUEST"
+  #hash_key     = "LockID"
 
+  #attribute {
+    #name = "LockID"
+    #type = "S"
+  #}
+
+  #tags = {
+    #Name        = "Terraform Lock Table"
+    #Environment = var.environment
+  #}
+#}
--- terraform/jenkin-ansible-server-tf/modules/bootstrap-backend/output.tf
+++ terraform/jenkin-ansible-server-tf/modules/bootstrap-backend/output.tf
@@ -2,6 +2,6 @@ output "state_bucket_name" {
   value = aws_s3_bucket.tf-state-lock.id
 }
 
-output "lock_table_name" {
-  value = aws_dynamodb_table.tf_lock.name
-}
+#output "lock_table_name" {
+  #value = aws_dynamodb_table.tf_lock.name
+#}
